d5be2599d069149cd1bc1ab7ef210beb3d1192db
xLillium <nicolas.motillon@gmail.com>
2023-10-04 13:24:08 +0200

    add: create session endpoint

--- src/main/java/com/og/conferencedemo/controllers/SessionsController.java
+++ src/main/java/com/og/conferencedemo/controllers/SessionsController.java
@@ -3,10 +3,7 @@ package com.og.conferencedemo.controllers;
 import com.og.conferencedemo.models.Session;
 import com.og.conferencedemo.repositories.SessionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,10 @@ public class SessionsController {
     @RequestMapping("{id}")
     public Session get(@PathVariable Long id) {
         return sessionRepository.getReferenceById(id);
+    }
+
+    @PostMapping
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
     }
 }
